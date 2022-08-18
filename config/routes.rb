2041fae095b42d04545ad3aa119d75930b4a0c7c
Sacha Gotainer <sgotainer@hotmail.com>
2022-08-18 13:50:15 +0200

    All green

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :lists, only: %w[index show new create] do
+    resources :bookmarks, only: %w[new create]
+  end
+  resources :bookmarks, only: %w[destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
